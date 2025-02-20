c909be559bc2366a7cdfea7e5ce1e87a5233852e
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-20 21:54:10 +0500

    add

---
+++ app.js
@@ -0,0 +1,9 @@
+// let user1 = prompt ("your first name")
+// let user2 = prompt("your last name")
+// let fullName = user1+ ""+ user2
+// alert (fullName);
+// let user = prompt("your fav mobile phone model")
+// document.write(user.length)
+// let cast = "pakistani"
+// let index = cast.indexOf("n")
+// document.write("the index of n is "+ ""+index)
