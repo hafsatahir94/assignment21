94f2bc1b99fa6ca600d22995cb3fcbe7603b3421
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-26 10:11:34 +0500

    commit

--- app.js
+++ app.js
@@ -7,3 +7,30 @@
 // let cast = "pakistani"
 // let index = cast.indexOf("n")
 // document.write("the index of n is "+ ""+index)
+// let word = "hello  world"
+// let last = word.lastIndexOf("l")
+// document.write ("the last index of the word is"+ last)
+// let charc = "pakistani"
+// let charAt= charc.charAt(3)
+// document.write(charAt)
+// let user1 = prompt ("your first name")
+// let user2 = prompt("your last name")
+// let fullName = user1+ ""+ user2
+// alert (fullName);
+// let city = "hyderabad"
+// let replace = city.replace("hyder", "islam")
+// document.write(replace)
+
+// let message1 = "Ali and Sami are best friends" 
+// let message2 ="They play cricket and football together"
+// let replace1 = message1.replace("and","&")
+// let replace2 = message2.replace("and","&")
+
+
+// document.write(replace1+replace2)
+
+// let str = "472";
+// let num = Number(str);  
+// document.write(typeof num)
+let user = prompt("please input peanuts")
+let upper =document.write(user.toUpperCase())
